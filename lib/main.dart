da66de622a5d2689248fca93a10c3c97dd66925f
lucaslopesxx <lucasalvarengalopes@gmail.com>
2023-06-21 22:10:02 -0300

    Implementação de indicador de posição do carrossel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -8,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,33 +18,89 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text("WinchesterAPP"),
+          appBar: AppBar(
+              title: const Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('WinchesterAPP'),
+              // Image.asset(
+              //   'assets/images/icone.png',
+              //   width: 100,
+              // ),
+            ],
+          )),
+          body: ListView(
+            children: [
+              Column(children: [
+                Carrossel(),
+              ])
+            ],
+          ),
+        ));
+  }
+}
+
+class Carrossel extends StatefulWidget {
+  const Carrossel({super.key});
+
+  @override
+  State<Carrossel> createState() => _Carrossel();
+}
+
+class _Carrossel extends State<Carrossel> {
+  int _atual = 0;
+  final CarouselController _controle = CarouselController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      CarouselSlider(
+        items: modulos.asMap().entries.map((i) {
+          final int posicao = i.key;
+          final String modulo = i.value;
+          final String imagem = modImagens[posicao];
+
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                child: Card(modulo, imagem),
+              );
+            },
+          );
+        }).toList(),
+        carouselController: _controle,
+        options: CarouselOptions(
+          enlargeCenterPage: true,
+          onPageChanged: (posicao, reason) {
+            setState(() {
+              _atual = posicao;
+            });
+          },
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: modulos.asMap().entries.map((i) {
+          return GestureDetector(
+            onTap: () => _controle.animateToPage(i.key),
+            child: Container(
+              width: 12.0,
+              height: 12.0,
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 3.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: (Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black)
+                    .withOpacity(_atual == i.key ? 0.9 : 0.4),
+              ),
             ),
-            body: ListView(
-              scrollDirection: Axis.vertical,
-              children: [
-                Column(children: [
-                  CarouselSlider(
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      autoPlayInterval: const Duration(seconds: 10),
-                      enlargeCenterPage: true,
-                    ),
-                    items: [0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 1.0),
-                              child: (Card(modulos[i], modImagens[i])));
-                        },
-                      );
-                    }).toList(),
-                  )
-                ])
-              ],
-            )));
+          );
+        }).toList(),
+      ),
+    ]);
   }
 }
 
@@ -68,19 +125,25 @@ class Card extends StatelessWidget {
         child: Stack(
           alignment: AlignmentDirectional.center,
           children: [
-            Image.asset(
-              'assets/image/$imagem',
-              width: 125,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.asset(
+                  'assets/images/$imagem',
+                  height: 100,
+                ),
+                Text(
+                  modulo,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromARGB(255, 0, 199, 255),
+                    fontSize: 28,
+                    decoration: TextDecoration.none,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
             ),
-            Text(
-              modulo,
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                color: Color.fromARGB(255, 0, 199, 255),
-                fontSize: 28,
-              ),
-              textAlign: TextAlign.center,
-            )
           ],
         ));
   }
